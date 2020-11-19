4598927365705a33e0dae56d347737b53f088be1
Elias F. Fysken <git@fyksen.net>
2020-11-19 01:34:35 +0000

    Update install_pkg function in conf.sh

--- conf.sh
+++ conf.sh
@@ -1,7 +1,11 @@
 #!/bin/env sh
 
 # Configured from https://wiki.osdev.org/GCC_Cross-Compiler#Installing_Dependencies
-export INSTALL_PKG="yay -Syu"
+
+function INSTALL_PKG {
+    yay -Syu $1
+}
+
 export DEPENDENCIES="base-devel gmp libmpc mpfr"
 
 export PREFIX="$HOME/opt/cross"
